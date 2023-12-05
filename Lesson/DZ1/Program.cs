04ac6832edb7255a80887a5fbed925b2a8a95044
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-05 18:33:06 +0300

    домашняя задача1

---
+++ Lesson/DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.WriteLine("Число является кратным как 7, так и 23.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является кратным как 7, так и 23.");
+        }
+        
+        Console.ReadLine();
+
