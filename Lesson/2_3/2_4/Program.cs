dd86c32db3a2de974742189147c9573aef191daa
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-05 18:13:14 +0300

    Classwork 2_4

---
+++ Lesson/2_3/2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿void ThirdNum(int num)
+{
+    if (num<100)
+    {
+        Console.WriteLine("There is no third digit");
+        return;
+    }
+    while (num>999)
+        num/=10;
+    Console.WriteLine (num%10);    
+    {
+        
+    }
+}
+
+int a = int.Parse(Console.ReadLine()!);
+ThirdNum(a);
