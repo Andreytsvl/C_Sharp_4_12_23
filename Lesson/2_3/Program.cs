9da6b2930ddd8d631863d0576dc2357b495ce976
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-05 17:47:00 +0300

    Classwork 2_3

---
+++ Lesson/2_3/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if(a%b==0)
+    Console.WriteLine("Multiply");
+else
+    Console.WriteLine($"Not multyply {a} % {b}");  
+    
