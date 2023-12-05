217607c68eff8bd103a56d7c444f2ba09bb2684f
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-05 18:53:14 +0300

    домашняя задача3

---
+++ Lesson/DZ3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход целое число из отрезка [10;99]
+// выводит наибольшую цифру из числа
+  Console.WriteLine("Введите число от 10 до 99:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int maxDigit = 1;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            if (digit > maxDigit)
+            {
+                maxDigit = digit;
+            }
+
+            number /= 10;
+        }
+
+        Console.WriteLine("Наибольшая цифра в числе: " + maxDigit);
