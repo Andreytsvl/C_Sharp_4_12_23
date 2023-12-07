9f4a136aa883a7f3826bd3aeca4a0510bb7ae431
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-07 08:16:49 +0300

    слияние веток

---
+++ Lesson/DZ4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+//а на выходе показывает его цифры через запятую.
+ Console.WriteLine("Введите натуральное число:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Цифры числа {0} через запятую: ", n);
+
+        string digits = string.Empty;
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+            digits = digit + "," + digits;
+            n /= 10;
+        }
+
+        digits = digits.TrimEnd(',');
+
+        Console.WriteLine(digits);
+
+        Console.ReadLine();
+    
+
